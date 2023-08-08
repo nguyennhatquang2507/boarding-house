8bdaf0ea0456eefcc87e61513ab2353f6dc28f25
Nguyễn Nhật Quang <nguyennhatquang2507@gmail.com>
2023-08-08 11:46:48 +0700

    Initial Project

---
+++ src/main/java/com/kiran/boardinghouse/BoardingHouseApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.boardinghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/boardinghouse/BoardingHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.boardinghouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
